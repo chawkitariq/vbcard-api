971d55cc7fa7f81d35a26802a0e349ad0544083f
chawkitariq <chawkitariq@icloud.com>
2024-08-31 23:05:56 +0200

    feat(event): use AuthRegisterEvent

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,22 +4,29 @@ import { User } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { AuthRegisterDto } from './dto/register.dto';
 import { HashService } from 'src/hash/hash.service';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { AuthRegisterEvent } from './events/register.event';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
-    private readonly hashService: HashService
+    private readonly hashService: HashService,
+    private readonly eventEmitter: EventEmitter2
   ) {}
 
   async register({ email, password }: AuthRegisterDto) {
     const hashedPassword = await this.hashService.hash(password);
 
-    return this.userService.create({
+    const user = await this.userService.create({
       email,
       password: hashedPassword
     });
+
+    this.eventEmitter.emit(AuthRegisterEvent.name, new AuthRegisterEvent(user.id));
+
+    return user;
   }
 
   async login({ id, email }: User) {
