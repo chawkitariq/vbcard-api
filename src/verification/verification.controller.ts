128d16a959dfeaa351c67b34fdcbf51cb6d503ba
chawkitariq <chawkitariq@icloud.com>
2024-09-01 00:56:22 +0200

    feat(controller): setup verification controller

---
+++ src/verification/verification.controller.ts
@@ -0,0 +1,34 @@
+import { BadRequestException, Controller, Get, InternalServerErrorException, Query } from '@nestjs/common';
+import { VerificationService } from './verification.service';
+import { Verification } from './entities/verification.entity';
+
+@Controller('verifications')
+export class VerificationController {
+  constructor(private readonly verificationService: VerificationService) {}
+
+  @Get('verify')
+  async verifyUser(@Query('token') token: string) {
+    let verification: Verification | undefined;
+
+    try {
+      verification = await this.verificationService.findOneByToken(token);
+    } catch (error) {
+      throw new InternalServerErrorException('Something wrong');
+    }
+
+    if (!verification) {
+      throw new BadRequestException('Invalid token');
+    }
+
+    if (verification.isExpired()) {
+      throw new BadRequestException('Token expired');
+    }
+
+    try {
+      verification.verifiedAt = new Date();
+      await this.verificationService.update(verification.id, verification);
+    } catch (error) {
+      throw new InternalServerErrorException('Something wrong');
+    }
+  }
+}
--- src/verification/verification.module.ts
+++ src/verification/verification.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { VerificationService } from './verification.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Verification } from './entities/verification.entity';
+import { VerificationController } from './verification.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Verification])],
   providers: [VerificationService],
-  exports: [VerificationService]
+  exports: [VerificationService],
+  controllers: [VerificationController]
 })
 export class VerificationModule {}
