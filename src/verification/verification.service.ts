417b46d09ede3123b1422cfce9670420ee4a8a16
chawkitariq <chawkitariq@icloud.com>
2024-08-31 23:43:39 +0200

    feat(service): setup verification service

--- src/verification/verification.service.ts
+++ src/verification/verification.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Verification } from './entities/verification.entity';
+import { Repository } from 'typeorm';
+import { CreateVerificationDto } from './dto/create.dto';
+import { UpdateVerificationDto } from './dto/update.dto';
 
 @Injectable()
-export class VerificationService {}
+export class VerificationService {
+  constructor(
+    @InjectRepository(Verification)
+    private readonly verificationRepository: Repository<Verification>
+  ) {}
+
+  create(createVerificationDto: CreateVerificationDto) {
+    const verification = this.verificationRepository.create(createVerificationDto);
+    return this.verificationRepository.save(verification);
+  }
+
+  async update(id: string, updateVerificationDto: UpdateVerificationDto) {
+    return this.verificationRepository.update(id, updateVerificationDto);
+  }
+}
