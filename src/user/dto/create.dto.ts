4fa8246bcc20a00019e671151410391e8e8cb0c8
chawkitariq <chawkitariq@icloud.com>
2024-09-01 11:01:47 +0200

    feat(dto): improve user dto

--- src/user/dto/create.dto.ts
+++ src/user/dto/create.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+import { Exclude } from 'class-transformer';
+import { Verification } from 'src/verification/entities/verification.entity';
+
+export class CreateUserDto {
+  @Exclude()
+  email: string;
+
+  @Exclude()
+  password: string;
+
+  @Exclude()
+  verification?: Verification;
+}
