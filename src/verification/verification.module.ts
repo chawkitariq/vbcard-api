00d7678c1e6103caf312363537987c034f8f4074
chawkitariq <chawkitariq@icloud.com>
2024-09-01 11:02:40 +0200

    feat(module): use throttler by verification module

--- src/verification/verification.module.ts
+++ src/verification/verification.module.ts
@@ -3,9 +3,18 @@ import { VerificationService } from './verification.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Verification } from './entities/verification.entity';
 import { VerificationController } from './verification.controller';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Verification])],
+  imports: [
+    TypeOrmModule.forFeature([Verification]),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 3
+      }
+    ])
+  ],
   providers: [VerificationService],
   exports: [VerificationService],
   controllers: [VerificationController]
