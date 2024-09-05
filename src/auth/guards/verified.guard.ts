df14f05d60171f76020a217bfd319062d98133ca
chawkitariq <chawkitariq@icloud.com>
2024-09-05 23:11:21 +0200

    feat(guard): improve AuthVerifiedGuard

--- src/auth/guards/verified.guard.ts
+++ src/auth/guards/verified.guard.ts
@@ -1,11 +1,28 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException
+} from '@nestjs/common';
 import { User } from 'src/user/entities/user.entity';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthVerifiedGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {
+  constructor(private readonly userService: UserService) {}
+
+  async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
-    const user = request.user as User;
+    const { id: userId } = request.user;
+
+    let user: User | null;
+
+    try {
+      user = await this.userService.findOne(userId);
+    } catch (error) {
+      throw new InternalServerErrorException('Something wrong');
+    }
 
     if (!user?.isVerified()) {
       throw new UnauthorizedException('User not verified');
