43e5a8efabf90882db55fe343033105ec80a4288
chawkitariq <chawkitariq@icloud.com>
2024-09-06 19:37:16 +0200

    feat(dto): define author field

--- src/contact/dto/create.dto.ts
+++ src/contact/dto/create.dto.ts
@@ -2,11 +2,12 @@ import { IsOptional, IsString, IsUUID } from 'class-validator';
 import { IsExist } from 'src/constraint/is-exist.constraint';
 import { IsVcf } from 'src/constraint/is-vcf.constraint';
 import { File } from 'src/file/entities/file.entity';
+import { User } from 'src/user/entities/user.entity';
 
 export class CreateContactDto {
   @IsOptional()
   @IsString()
-  @IsVcf("4.0")
+  @IsVcf('4.0')
   vcf?: string;
 
   @IsOptional()
@@ -16,4 +17,5 @@ export class CreateContactDto {
   photoId?: string;
 
   photo?: File;
+  author?: User;
 }
