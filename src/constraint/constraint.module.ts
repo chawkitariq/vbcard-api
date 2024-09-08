771e3b49db372f9459548c2070cf6015842259b4
chawkitariq <chawkitariq@icloud.com>
2024-09-08 14:02:32 +0200

    feat(constraint): setup IsManyExistConstraint

--- src/constraint/constraint.module.ts
+++ src/constraint/constraint.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { IsExistConstraint } from './is-exist.constraint';
 import { IsOneExistConstraint } from './is-one-exist.constraint';
+import { isManyExistConstraint } from './is-many-exist.constraint';
 
 @Module({
-  providers: [IsExistConstraint, IsOneExistConstraint],
-  exports: [IsExistConstraint, IsOneExistConstraint]
+  providers: [IsExistConstraint, IsOneExistConstraint, isManyExistConstraint],
+  exports: [IsExistConstraint, IsOneExistConstraint, isManyExistConstraint]
 })
 export class ConstraintModule {}
---
+++ src/constraint/is-many-exist.constraint.ts
@@ -0,0 +1,51 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments
+} from 'class-validator';
+import { Injectable } from '@nestjs/common';
+import { EntityTarget, FindManyOptions, DataSource } from 'typeorm';
+
+@ValidatorConstraint({ name: 'isManyExistConstraint', async: true })
+@Injectable()
+export class isManyExistConstraint<Entity> implements ValidatorConstraintInterface {
+  constructor(private dataSource: DataSource) {}
+
+  async validate(value: unknown, args: ValidationArguments): Promise<boolean> {
+    const [entityClass, findCallback]: [EntityTarget<Entity>, (value: unknown) => FindManyOptions<Entity>] =
+      args.constraints as [EntityTarget<Entity>, (value: unknown) => FindManyOptions<Entity>];
+
+    const repository = this.dataSource.getRepository(entityClass);
+
+    const findOptions = findCallback(value);
+
+    try {
+      const entities = await repository.find(findOptions);
+      return entities.length > 0;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `${args.property} does not exist!`;
+  }
+}
+
+export function IsManyExist<Entity>(
+  entity: EntityTarget<Entity>,
+  findCallback: (value: unknown) => FindManyOptions<Entity>,
+  validationOptions?: ValidationOptions
+) {
+  return function (object: unknown, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [entity, findCallback] as [EntityTarget<Entity>, (value: unknown) => FindManyOptions<Entity>],
+      validator: isManyExistConstraint<Entity>
+    });
+  };
+}
