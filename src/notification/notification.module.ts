8120da4ece314eb5a781de0d92d7b4ba5461a7b0
chawkitariq <chawkitariq@icloud.com>
2024-09-15 09:56:31 +0200

    fix(module): import UserModule

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -3,9 +3,10 @@ import { NotificationService } from './notification.service';
 import { NotificationController } from './notification.controller';
 import { Notification } from './entities/notification.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification])],
+  imports: [TypeOrmModule.forFeature([Notification]), UserModule],
   controllers: [NotificationController],
   providers: [NotificationService]
 })
